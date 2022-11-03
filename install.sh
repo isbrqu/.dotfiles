05134efefe8fa7ff4d07352c27c3f7f06789db29
Isaac Bruno Quiroga <isbrqu@gmail.com>
2022-11-02 23:01:54 -0300

    comienza a arreglar algunas cosas

--- install.sh
+++ install.sh
@@ -3,17 +3,17 @@
 declare path="$(realpath $0)"
 declare src="${path%/*}/src"
 declare conf="$HOME/.config"
-declare dest="$conf/d"
+declare dest="$conf/dotfiles"
 
-ln  --symbolic  $src                       $dest
-ln  --symbolic  $dest/bash/run             $HOME/.bashrc
-ln  --symbolic  $dest/bash/logout          $HOME/.bash_logout
-ln  --symbolic  $dest/bash/profile         $HOME/.profile
-ln  --symbolic  $dest/visidata/config      $HOME/.visidatarc
-ln  --symbolic  $dest/visidata             $HOME/.visidata
-ln  --symbolic  $dest/script/src           $HOME/.script
-ln  --symbolic  $dest/tmux                 $conf/tmux
-ln  --symbolic  $dest/misc/user-dirs.dirs  $conf/user-dirs.dirs
-ln  --symbolic  $dest/i3wm                 $conf/i3
-ln  --symbolic  $dest/git                  $conf/git
-ln  --symbolic  $dest/misc/mimeapp.list    $conf/mimeapp.list
+ln --no-target-directory --symbolic  $src                       $dest
+# ln  --symbolic  $dest/bash/run             $HOME/.bashrc
+# ln  --symbolic  $dest/bash/logout          $HOME/.bash_logout
+# ln  --symbolic  $dest/bash/profile         $HOME/.profile
+# ln  --symbolic  $dest/visidata/config      $HOME/.visidatarc
+# ln  --symbolic  $dest/visidata             $HOME/.visidata
+# ln  --symbolic  $dest/script/src           $HOME/.script
+# ln  --symbolic  $dest/tmux                 $conf/tmux
+# ln  --symbolic  $dest/misc/user-dirs.dirs  $conf/user-dirs.dirs
+# ln  --symbolic  $dest/i3wm                 $conf/i3
+ln --no-target-directory --symbolic  $dest/git                  $conf/git
+# ln  --symbolic  $dest/misc/mimeapp.list    $conf/mimeapp.list
