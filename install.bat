b8b7fe7e669382e8bdec0392560f81712bf0733a
Isaac Bruno Quiroga <isbrqu@gmail.com>
2023-02-22 13:00:32 -0300

    upt: link the script src folder

--- install.bat
+++ install.bat
@@ -3,6 +3,7 @@
 mklink /d %USERPROFILE%\.dotfiles %~dp0%src
 mklink /d %USERPROFILE%\.vim %USERPROFILE%\.dotfiles\vim
 mklink /d %USERPROFILE%\.aws %USERPROFILE%\.dotfiles\aws
+mklink /d %USERPROFILE%\.script %USERPROFILE%\.dotfiles\script\src
 xcopy %USERPROFILE%\.dotfiles\ssh %USERPROFILE%\.ssh /i /e /c
 
 rmdir /s /q %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
