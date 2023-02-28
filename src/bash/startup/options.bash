f375a4c017efb226e4062e53953a2d42e4508ae5
Isaac Bruno Quiroga <isaacbrunosecu@gmail.com>
2023-02-28 09:09:45 -0300

    rmv: elmina una opción redundante

--- src/bash/startup/options.bash
+++ src/bash/startup/options.bash
@@ -1,5 +1,3 @@
 # check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
 shopt -s checkwinsize
-# append to the history file, don't overwrite it
-shopt -s histappend
